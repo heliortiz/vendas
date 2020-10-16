635bcd85c1b3dc49b23d5e850f7aa1877d6b0bf4
Helio Ortiz <heliortiz@gmail.com>
2020-10-15 21:28:04 -0300

    Project created

---
+++ src/test/java/com/hodv/vendas/VendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hodv.vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
