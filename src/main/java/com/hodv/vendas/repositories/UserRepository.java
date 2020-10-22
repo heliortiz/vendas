f0a0d2aecb6ce4aee0a3bcd605b30eeb43d8ff28
Helio Ortiz <heliortiz@gmail.com>
2020-10-21 21:40:46 -0300

    Service layer, component registration

--- src/main/java/com/hodv/vendas/repositories/UserRepository.java
+++ src/main/java/com/hodv/vendas/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.hodv.vendas.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.hodv.vendas.entities.User;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
